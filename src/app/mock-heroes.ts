0ead0bda310e298c1a084d2596f3987e9c8fedbd
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2021-02-18 12:47:43 +0100

    add Details Heroes component - 2.paragraph

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Yoda'},
+  {id: 12, name: 'Luke Skywalker'},
+  {id: 13, name: 'Obi-Wan Kenobi'},
+  {id: 14, name: 'Darth Vader'},
+  {id: 15, name: 'Anakin Skywalker'},
+  {id: 16, name: 'Leia Organa'},
+  {id: 17, name: 'R2D2'},
+  {id: 18, name: 'C3PO'},
+  {id: 19, name: 'Jar Jar Binks'},
+  {id: 20, name: ' Death Star'}
+];
