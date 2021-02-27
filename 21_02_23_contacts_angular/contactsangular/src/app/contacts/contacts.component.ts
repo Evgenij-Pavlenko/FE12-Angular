3c4d13ce82a6ea63eaad69f08ee77570cddd68f3
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2021-02-27 23:20:23 +0100

    contact angular app

---
+++ 21_02_23_contacts_angular/contactsangular/src/app/contacts/contacts.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {Contact} from '../model/Contact';
+import {ContactService} from '../service/contact.service';
+
+@Component({
+  selector: 'app-contacts',
+  templateUrl: './contacts.component.html',
+  styleUrls: ['./contacts.component.css']
+})
+export class ContactsComponent implements OnInit {
+  contacts: Contact[] = [];
+
+  constructor(private contactService: ContactService) { }
+
+  ngOnInit(): void {
+    this.getContacts();
+  }
+
+  private getContacts(): void {
+    this.contactService.getContacts().subscribe( contacts => this.contacts = contacts);
+  }
+
+  onDelete(contact: Contact): void {
+    this.contactService.deleteContact(contact.id).subscribe(
+      _ => this.contacts = this.contacts.filter(value => {
+        return value.id !== contact.id;
+      })
+    );
+  }
+}
